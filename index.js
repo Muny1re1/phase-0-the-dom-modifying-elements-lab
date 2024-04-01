166e3c29c25011ce10c152a565e846249fbf9ab9
Muny1re1 <munyiriandy@gmail.com>
2024-04-01 18:31:16 +0300

    DOM modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const body = document.body;
+const main = document.getElementById("main");
+body.removeChild(main);
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Andy is the champion"
